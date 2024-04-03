9e00fe9e5f7281738301c306175fda1a96d9c170
Neeraj kumar <neerajkumar08697@gmail.com>
2024-04-03 22:32:24 +0530

    feat: quantity toggle

---
+++ homeQuantityToggle.js
@@ -0,0 +1,33 @@
+
+export const homeQuantityToggle = (id, event, stock) => {
+
+
+    const currentCardElement = document.querySelector(`#card${id}`);
+    const productQuantity = currentCardElement.querySelector(".productQuantity");
+
+    let quantity = parseInt(productQuantity.getAttribute("data-quantity")) || 1;
+
+    if (event.target.className === "cartIncrement") {
+        if (quantity < stock) {
+            quantity++;
+            console.log(quantity);
+        }
+        else if (quantity === stock) {
+            quantity = stock;
+        }
+    }
+    else if (event.target.className === "cartDecrement") {
+        if (quantity > 1) {
+            quantity -= 1;
+        }
+    }
+
+
+    console.log(quantity);
+    console.log(productQuantity);
+
+    productQuantity.innerText = quantity;
+
+    productQuantity.setAttribute("data-quantity", quantity.toString());
+
+}
